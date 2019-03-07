25e8ee22c5d463606a800414a4370c97422ac53a
Aditya Raj <aditya.x510@gmail.com>
2019-03-07 23:40:19 +0530

    initial

--- SecondSemester/Java/stringHandling/TestStringConstructor.java
+++ SecondSemester/Java/stringHandling/TestStringConstructor.java
@@ -1,10 +1,10 @@
 class TestStringConstructor{
-   
     public static void main(String[] args) {
         // default cons
         String str1 = new String();
-        String str2 = new String("");
-        System.out.println(str2.length());
+        String str2 = new String("with parameter");
+        System.out.println(str1);
+        System.out.println(str2);
         
         }
 }
