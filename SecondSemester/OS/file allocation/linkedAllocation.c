3f7fae8655f8a9a327ecadc5751c1a6a9b452eb6
Aditya Raj <aditya.x510@gmail.com>
2019-06-05 15:42:11 +0530

    init

--- SecondSemester/OS/file allocation/linkedAllocation.c
+++ SecondSemester/OS/file allocation/linkedAllocation.c
@@ -2,47 +2,56 @@
 #include<stdlib.h>
 void main()
 {
-int f[50], index[50],i, n, st, len, j, c, k, ind,count=0;
-for(i=0;i<50;i++)
-f[i]=0;
-x:printf("Enter the index block: ");
-scanf("%d",&ind);
-if(f[ind]!=1)
-{
-printf("Enter no of blocks needed and no of files for the index %d on the disk : \n", ind);
-scanf("%d",&n);
-}
-else
-{
-printf("%d index is already allocated \n",ind);
-goto x;
-}
-y: count=0;
-for(i=0;i<n;i++)
-{
-scanf("%d", &index[i]);
-if(f[index[i]]==0)
-count++;
-}
-if(count==n)
-{
-for(j=0;j<n;j++)
-f[index[j]]=1;
-printf("Allocated\n");
-printf("File Indexed\n");
-for(k=0;k<n;k++)
-printf("%d-------->%d : %d\n",ind,index[k],f[index[k]]);
-}
-else
-{
-printf("File in the index is already allocated \n");
-printf("Enter another file indexed");
-goto y;
-}
-printf("Do you want to enter more file(Yes - 1/No - 0)");
-scanf("%d", &c);
-if(c==1)
-goto x;
-else
-exit(0);
+    int f[20], index[20],i, n, st, len, j, c, k, ind,count=0,size;
+    for(i=0;i<20;i++)
+    f[i]=0;
+    x:printf("Enter the starting block: ");
+    scanf("%d",&ind);
+    if(f[ind]!=1)
+    {
+        printf("\nEnter block size for current file\n");
+        scanf("%d",&size);
+        printf("Enter no of blocks needed ");
+        printf("and also enter the files for the index %d on the disk : \n", ind);
+        scanf("%d",&n);
+        if(n>size){
+            printf("\nBlock count/size exceded!!!!! give less than %d ",size);
+            exit(0);
+        }
+    }
+    else
+    {
+        printf("\n%d index is already allocated \n",ind);
+        goto x;
+    }
+    y: count=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d", &index[i]);
+        if(f[index[i]]==0)
+        count++;
+    }
+    if(count==n)
+    {
+        for(j=0;j<n;j++)
+            f[index[j]]=1;
+        printf("Allocated\n");
+        printf("File Indexed\n");
+        printf("\nstart >> %d ",ind);
+        for(k=0;k<n;k++)            
+            printf("-->%d ",index[k]);
+        printf(">>end");
+    }
+    else
+    {
+        printf("\nFile in the index is already allocated \n");
+        printf("\nEnter another file indexed");
+        goto y;
+    }
+    printf("\nDo you want to enter more file(Yes - 1/No - 0)");
+    scanf("%d", &c);
+    if(c==1)
+        goto x;
+    else
+        exit(0);
 }
