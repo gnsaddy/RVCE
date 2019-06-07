2c72f4a11e4cc2f0a98e2ad1118624e44a584f42
Aditya Raj <aditya.x510@gmail.com>
2019-06-07 08:59:05 +0530

    init

--- SecondSemester/Java/LabPrograms/src/testfiles/FileOperation.java
+++ SecondSemester/Java/LabPrograms/src/testfiles/FileOperation.java
@@ -1,62 +1,64 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.File;
-import java.util.*;
+package testfiles;
 
-class InputOutputStream {
-    
-    public static void fileDetails(){
-        File dir = new File("./.");
-        String[] children = dir.list();
-        if (children == null) {
-            System.out.println( "Either dir does not exist or is not a directory");
-        } else { 
-            for (int i = 0; i< children.length; i++) {
-                String filename = children[i];
-                System.out.println(filename);
+import java.io.*;
+import java.util.Scanner;
+
+public class FileOperation {
+    public static void fileDetails() {
+        File file = new File("./.");
+        String[] child = file.list();
+        if (child == null) {
+            System.out.println("Not found");
+        } else {
+            for (int i = 0; i < child.length; i++) {
+                String fileName = child[i];
+                System.out.println(fileName);
             }
         }
+
     }
+
+
     public static void writeToFile() {
-        System.out.println("select from the following files :-");
+        System.out.println("Select from the following file : ");
         fileDetails();
+
         try{
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter existing file name or enter file name to create file");
+            System.out.println("Enter the existing file name or create new file");
             String fileName = sc.nextLine();
-            FileWriter fileWriter = new FileWriter(fileName);
-            System.out.println("--------------------------------");
-            System.out.println("Enter data ----------------");
+            System.out.println("Enter string to input ");
             String str = sc.nextLine();
-            // byte[] strToByte = str.getBytes();
+            FileWriter fileWriter = new FileWriter(fileName);
             fileWriter.write(str);
             fileWriter.close();
-            System.out.println("Operation sucessful"); 
-        }catch(IOException e){
-            System.out.println("Exception caught "+e);
+            System.out.println("Operation done");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public static void readAFile() {
+    public static void readAFile(){
+        System.out.println("Select from the following file");
         fileDetails();
+
         try{
-            System.out.println("Choose for the following files :- ");
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter selected file name");
-            String fileName = sc.nextLine();
-            FileReader fileReader = new FileReader(fileName);
+            System.out.println("Enter the file name");
+            String filename = sc.nextLine();
+            FileReader fileReader = new FileReader(filename);
             int i=0;
-            while((i=fileReader.read())!=-1)
+            while ((i=fileReader.read()) != -1){
                 System.out.print((char)i);
+            }
             fileReader.close();
-        }catch(IOException e){
-            System.out.println("Exception caught "+e);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-    
+
     public static void appendToFile(){
         System.out.println("select from the following files :-");
         fileDetails();
@@ -75,7 +77,6 @@ class InputOutputStream {
         }catch(IOException e){
             System.out.println("Exception caught "+e);
         }
-
     }
 
     public static void main(String[] args) {
@@ -87,7 +88,7 @@ class InputOutputStream {
             System.out.print("Enter choice : \n");
             ch = sc.nextInt();
             switch(ch){
-                case 1: 
+                case 1:
                     fileDetails();
                     break;
                 case 2:
