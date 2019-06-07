c05f51a80f8e1eb049cb5298919b0b64ef0f27ba
Aditya Raj <aditya.x510@gmail.com>
2019-06-07 10:31:15 +0530

    init

---
+++ SecondSemester/Java/LabPrograms/src/CustomException.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class UsnNotFound extends Exception {
+
+    public UsnNotFound(String message) {
+        super(message);
+    }
+
+}
+
+class StudentUsn {
+
+     public static void usn(String name) throws UsnNotFound {
+
+         if ("1RD18MCA01".equalsIgnoreCase(name) || "1RD18MCA02".equalsIgnoreCase(name)) {
+             System.out.println("USN valid! You scored first Rank");
+         }
+         else if (name != "1RD18MCA01" || name != "1RD18MCA02"){
+             throw new UsnNotFound("Result not found!");
+         }
+         else if ("".equals(name)){
+             throw new UsnNotFound("USN format {1RD__MCA__} ");
+         }
+     }
+
+     public static void main(String[] args) {
+         try {
+             Scanner sc = new Scanner(System.in);
+             System.out.println("Enter USN to check result");
+             String take = sc.nextLine();
+             usn(take);
+
+         } catch (UsnNotFound e) {
+             System.out.println("Exception : "+e);
+         }
+     }
+ }
