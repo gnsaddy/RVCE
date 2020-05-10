f00a56a3ce5afc3f1fe5a5a450a430cab25e8e57
Aditya Raj <aditya.x510@gmail.com>
2020-05-10 19:05:06 +0530

    update

--- FourthSemester/androidDevelopment/stockManagement/public/js/main.js
+++ FourthSemester/androidDevelopment/stockManagement/public/js/main.js
@@ -6,26 +6,62 @@ let stock = [
 	{ pid: 5, p_name: 'Puzzle Box', p_stock: 10 },
 ];
 
-let dataKey = Object.keys(stock[0]);
-// console.log(dataKey.length);
-
 function generateTable(tbody, data) {
 	for (let element of data) {
-		// console.log(element.pid);
-		// console.log(element.p_name);
-		// console.log(element.p_stock);
-
 		let row = tbody.insertRow(); // tr
-		console.log(row);
 
 		for (key in element) {
 			let cell = row.insertCell(); // td
-			// console.log(cell);
 			let text = document.createTextNode(element[key]);
 			cell.appendChild(text);
 		}
+
+		let cellInc = row.insertCell();
+		let textInc = document.createElement('input');
+		textInc.setAttribute('type', 'number');
+		textInc.className = 'rounded border border-info inc';
+		let btnInc = document.createElement('button');
+		btnInc.className = 'btn btn-sm btn-info ml-2 mb-2 mt-1 btnInc';
+		btnInc.setAttribute('value', 'submit');
+		btnInc.innerHTML = 'Inc';
+		cellInc.appendChild(textInc);
+		cellInc.appendChild(btnInc);
+
+		let cellDec = row.insertCell();
+		let textDec = document.createElement('input');
+		textDec.setAttribute('type', 'number');
+		textDec.className = 'rounded border border-info dec';
+		let btnDec = document.createElement('button');
+		btnDec.setAttribute('value', 'submit');
+		btnDec.className = 'btn btn-sm btn-info ml-2 mb-2 mt-1 btnDec';
+		btnDec.innerHTML = 'Dec';
+		cellDec.appendChild(textDec);
+		cellDec.appendChild(btnDec);
 	}
 }
 
 let tbody = document.querySelector('tbody');
 generateTable(tbody, stock);
+
+function getValue() {
+	let incValue = document.getElementsByClassName('inc');
+	for (let i = 0; i < incValue.length; i++) {
+		let id = document.getElementById('inc' + (i + 1)).value;
+		if (stock[i].pid == i + 1 && id != '') {
+			stock[i].p_stock += Number(id);
+			console.log(stock[i].p_stock);
+			alert('Stock updated final count is ' + stock[i].p_stock);
+			window.location = '../index.html';
+		}
+	}
+}
+
+let b = document.getElementsByClassName('btnInc');
+let t = document.getElementsByClassName('inc');
+for (let i = 0; i < t.length; i++) {
+	t[i].setAttribute('id', 'inc' + (i + 1));
+}
+for (let i = 0; i < b.length; i++) {
+	b[i].setAttribute('id', 'idInc' + (i + 1));
+	b[i].setAttribute('onclick', 'getValue()');
+}
